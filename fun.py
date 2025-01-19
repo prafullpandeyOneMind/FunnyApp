757d402d08d5732771553b68d361e07e5bee60f7
Prafull Kumar Pandey <pkumar1@onemindservices.com>
2025-01-19 17:49:20 +0530

    bug fun.py

--- fun.py
+++ fun.py
@@ -1,16 +1,5 @@
-code = """
+a = d
+
 def example_function():
     x = 10
     return y  # undefined variable
-"""
-
-results = analyze_code(code)
-print(results)
-
-print(x)
-this is error also
-
-def this():
-    pass
-this line is also error
-print(z)
